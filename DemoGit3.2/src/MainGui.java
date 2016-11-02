2ea3c2d50c0440de9b6920e54055d4c9f6209a39
Khai <manhtruong531010@gmail.com>
2016-11-02 15:38:20 +0700

    init Project

---
+++ DemoGit3.2/src/MainGui.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+
+public class MainGui extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MainGui f = new MainGui();
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+
+}
